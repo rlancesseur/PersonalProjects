bbe917d52ca79860812e1e7814320c5f65254e24
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-28 11:32:55 +0100

    Update Pick items

--- PloufPlouf/assets/script.js
+++ PloufPlouf/assets/script.js
@@ -4,21 +4,33 @@ const btnCoin = document.querySelector("#btnCoin")
 const btnDice = document.querySelector("#btnDice")
 
 const containerMenu = document.querySelector("#containerMenu")
+const containerPickItems = document.querySelector("#containerPickItems")
 const containerRandomNumbers = document.querySelector("#containerRandomNumbers")
 const containerCoinFlip = document.querySelector("#containerCoinFlip")
 const containerRollDice = document.querySelector("#containerRollDice")
 
 const input1 = document.querySelector("#input1")
 const input2 = document.querySelector("#input2")
+const addItem = document.querySelector("#addItem")
+const listItems = document.querySelector("#listItems")
+const btnAdd = document.querySelector("#btnAdd")
+const btnPick = document.querySelector("#btnPick")
 const btnPloufNbr = document.querySelector("#btnPloufNbr")
 const btnCoinFlip = document.querySelector("#btnCoinFlip")
 const btnRollDice = document.querySelector("#btnRollDice")
 const imgCoin = document.querySelector("#imgCoin")
 const imgDice = document.querySelector("#imgDice")
 
-const nbrSortie = document.querySelector(".nbrSortie")
-const zoneResultat = document.querySelector(".zoneResultat")
+const nbrSortieItems = document.querySelector("#nbrSortieItems")
+const nbrSortieNbr = document.querySelector("#nbrSortieNbr")
+const zoneResultatPick = document.querySelector("#zoneResultatPick")
+const zoneResultatNumbers = document.querySelector("#zoneResultatNumbers")
 
+
+btnPickItems.addEventListener("click", () => {
+    containerMenu.style.display = "none"
+    containerPickItems.style.display = "flex"
+})
 
 btnNbr.addEventListener("click", () => {
     containerMenu.style.display = "none"
@@ -34,6 +46,32 @@ btnDice.addEventListener("click", () => {
     containerMenu.style.display = "none"
     containerRollDice.style.display = "flex"
 })
+
+let tabItems = []
+let suiteItems = ""
+
+btnAdd.addEventListener("click", () => {
+    tabItems.push(addItem.value)
+    suiteItems += addItem.value + " "
+    listItems.innerHTML = suiteItems
+    addItem.value = ""
+    addItem.focus()
+})
+
+const pickItems = () => {
+    let suiteResultat = ""
+    if (nbrSortieItems.value < 1) nbrSortieItems.value = 1
+    if (tabItems.length === 0) {
+        zoneResultatPick.innerHTML = "Veuillez ajouter des éléments à la liste." 
+    } else {
+        for (let i = 0; i < nbrSortieItems.value; i++) {
+            const indexRandom = Math.floor(Math.random() * tabItems.length)
+            const resultat = tabItems[indexRandom]
+            suiteResultat += resultat + " "
+        }
+        zoneResultatPick.innerHTML = suiteResultat
+    }
+}
 
 const tirageNbr = () => {
     let min = 0
@@ -57,14 +95,14 @@ const tirageNbr = () => {
         min++
     }
 
-    if (nbrSortie.value < 1) nbrSortie.value = 1
+    if (nbrSortieNbr.value < 1) nbrSortieNbr.value = 1
 
-    for (let i = 0; i < nbrSortie.value; i++) {
+    for (let i = 0; i < nbrSortieNbr.value; i++) {
         const indexRandom = Math.floor(Math.random() * difference)
         const resultat = tabNbr[indexRandom]
         suiteResultat += resultat + " "
     }
-    zoneResultat.innerHTML = suiteResultat
+    zoneResultatNumbers.innerHTML = suiteResultat
 }
 
 const coinFlip = () => {
@@ -77,6 +115,7 @@ const rollDice = () => {
     imgDice.src = "assets/img/dice" + imgRandom + ".png"
 }
 
+btnPick.addEventListener("click", pickItems)
 btnPloufNbr.addEventListener("click", tirageNbr)
 btnCoinFlip.addEventListener("click", coinFlip)
 btnRollDice.addEventListener("click", rollDice)
